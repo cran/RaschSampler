8732c4fac1054d59f99502b1557e8b2d714e94d5
hatz <hatz@wuw.at>
2006-10-07 00:00:00 +0000

    version 0.8.0

---
+++ R/rsunpack.R
@@ -0,0 +1,35 @@
+"rsunpack" <-
+function(x,n,k,nwords,userfunc){
+     # check for NAs (-2^31 not defined in R) in simulated matrices
+     # set value to 0
+
+     nas<-FALSE
+     if (k>=32) {
+       idx<-(1:length(x))[is.na(x)]   # indexvector for NAs
+       nas<-(length(idx)>0)
+       x[idx]<-0
+     }
+
+     t<-vector(length=n*k)
+
+     # calls unpacking routine
+     out<-.Fortran("unpack",
+                 as.integer(x),
+                 as.integer(nwords),
+                 mat=as.integer(t),
+                 as.integer(n),
+                 as.integer(k)
+     )
+     m<-matrix(out$mat,nr=n)
+     # replace NAs with bitpattern corresponding to -2^31,
+     # i.e., 0 0 0.... 0 1
+     if (nas) {
+        idx1<-trunc(idx/nwords)+idx%%nwords  # index for rows
+        idx2<-32+32*(1-idx%%2)*(nwords-1)    # index for column
+        m[idx1,idx2]<-1
+     }
+     # calls user function to calculate statistic(s)
+     RET<-userfunc(m)
+     RET
+}
+
---
+++ R/summary.RSctr.R
@@ -0,0 +1,13 @@
+"summary.RSctr" <-
+function(object,...)
+{
+    cat("\nCurrent sampler control specifications in",
+           deparse(substitute(object)),":\n")
+    cat("\tburn_in = ",object$burn_in,"\n")
+    cat("\tn_eff = ",object$n_eff,"\n")
+    cat("\tstep = ",object$step,"\n")
+    cat("\tseed = ",object$seed,"\n")
+    cat("\ttfixed = ",object$tfixed,"\n\n")
+    invisible(object)
+}
+
